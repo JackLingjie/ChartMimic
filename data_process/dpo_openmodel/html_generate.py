39fa1051bd5aaabf9fc8f78e3f4baf4039247b44
lingjiejiang <lingjiejiang@stu.pku.edu.cn>
2025-01-26 03:30:09 +0000

    add bsz

--- data_process/dpo_openmodel/html_generate.py
+++ data_process/dpo_openmodel/html_generate.py
@@ -125,7 +125,7 @@ def main():
     print(f"Using model: {model_name}")
     
     # 每次处理批量数据时的逻辑
-    batch_size = 32
+    batch_size = 128
     revised_data = []
     for i in range(0, len(data), batch_size):
         batch_data = data[i:i + batch_size]
