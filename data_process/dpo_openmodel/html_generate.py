636d93111899e7dfd9edd452440b467d1edcf713
lingjiejiang <lingjiejiang@stu.pku.edu.cn>
2025-01-27 10:42:57 +0000

    bsz 12

--- data_process/dpo_openmodel/html_generate.py
+++ data_process/dpo_openmodel/html_generate.py
@@ -125,7 +125,7 @@ def main():
     print(f"Using model: {model_name}")
     
     # 每次处理批量数据时的逻辑
-    batch_size = 8
+    batch_size = 12
     revised_data = []
     for i in tqdm(range(0, len(data), batch_size), desc="Processing Batches", unit="batch"):
         batch_data = data[i:i + batch_size]
