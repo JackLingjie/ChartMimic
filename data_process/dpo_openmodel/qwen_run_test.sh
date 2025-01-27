e13600ab4aefedd4473c58e5a4cd4c13cca79553
lingjiejiang <lingjiejiang@stu.pku.edu.cn>
2025-01-27 08:23:00 +0000

    run 7B

---
+++ data_process/dpo_openmodel/qwen_run_test.sh
@@ -0,0 +1,1 @@
+python data_process/dpo_openmodel/html_generate.py --model_name Qwen2-VL-7B-Instruct --model_path "/mnt/lingjiejiang/textual_aesthetics/model_checkpoint/vlm_checkpoints/Qwen2-VL-7B-Instruct" 
